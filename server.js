3cb1b4489a974d6e1b2f17cf309cb20cab1a4d14
sahen <sahenshah95@gmail.com>
2025-07-19 14:43:17 +0100

    fix featured products for prod database

--- server.js
+++ server.js
@@ -297,7 +297,7 @@ app.post('/api/contact', async (req, res) => {
 app.get('/api/featured-products', async (req, res) => {
   if (dbType === 'pg') {
     try {
-      const result = await pool.query('SELECT id, images FROM products WHERE featured = 1 OR featured = true');
+      const result = await pool.query('SELECT id, images FROM products WHERE featured = TRUE');
       const products = result.rows
         .map(row => {
           try {
