179c4da2f169e50e7a363c446545bcee07c42b8f
sahen <sahenshah95@gmail.com>
2026-01-23 14:07:15 +0000

    fix data errors

--- api/products.js
+++ api/products.js
@@ -109,11 +109,9 @@ export default async function handler(req, res) {
 
       // Correct handling for sizesToStore
       let sizesToStore = sizes;
-      // if (typeof sizesToStore !== "string") {
+      // if (typeof sizesToStore !== 'string') {
       //   sizesToStore = JSON.stringify(sizesToStore);
       // }
-      // Remove all backslashes
-      sizesToStore = sizesToStore.replace(/\\/g, "");
 
       let keyFeaturesToStore = key_features;
       if (typeof keyFeaturesToStore !== "string") {
